0c371038891729dcb8b8e259931fc0c9d2ee7f51
Leo Torres <leo.htor@gmail.com>
2015-01-15 21:56:16 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Your code here
+window.addEventListener("load", function() {
+changeText();
+});
+
+function changeText () {
+	document.getElementById("essentials").style.backgroundColor = "yellow";
+	document.getElementById("greeting").innerHTML = "Hello Planet Earth";
+}
+
